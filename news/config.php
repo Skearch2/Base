acc9f79fdc7efa46f1ba1fd0e8fb109db535fcfa
Iftikhar Ejaz <ejaziftkhar@gmail.com>
2019-12-01 20:55:52 -0600

    Add config files for db connection.

---
+++ news/config.php
@@ -0,0 +1,23 @@
+<?php
+//Hoosk Database details
+define ('DB_HOST', 'localhost');
+//Username
+define ('DB_USERNAME', 'phpmyadmin');
+//Pass
+define ('DB_PASS', 'appfactory');
+//Database Name
+define ('DB_NAME', 'skearch_news');
+//Base URL
+define ('BASE_URL', '/skearch/news');
+//Email/Cookie URL
+define ('EMAIL_URL', '');
+
+//Skearch Database details
+define ('DB_DEFAULT_HOST', 'localhost');
+//Username
+define ('DB_DEFAULT_USERNAME', 'phpmyadmin');
+//Pass
+define ('DB_DEFAULT_PASS', 'appfactory');
+//Database Name
+define ('DB_DEFAULT_NAME', 'skearch_dev');
+?>
