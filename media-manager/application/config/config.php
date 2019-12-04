3b70b77b8f46acf71a718013b1a87bc57ccf5ca9
Skearch, LLC <43715242+Skearch2@users.noreply.github.com>
2019-12-04 15:25:33 -0600

    change baseurl

--- media-manager/application/config/config.php
+++ media-manager/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']    = 'https://localhost/skearch/media-manager/';
+$config['base_url']    = BASE_URL;
 
 /*
 |--------------------------------------------------------------------------
