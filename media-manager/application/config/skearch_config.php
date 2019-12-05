110b4e95cd733fce53c39589aec3355186e401cd
Skearch, LLC <43715242+Skearch2@users.noreply.github.com>
2019-12-04 23:08:16 -0600

    update db credentials and base url

--- media-manager/application/config/skearch_config.php
+++ media-manager/application/config/skearch_config.php
@@ -21,7 +21,7 @@
 | environments.
 |
 */
-define('BASE_URL', 'https://localhost/skearch/media-manager/');
+define('BASE_URL', 'https://dev.skearch.com/media-manager/');
 
 
 /*
@@ -44,6 +44,6 @@ define('BASE_URL', 'https://localhost/skearch/media-manager/');
 */
 
 define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'phpmyadmin');
-define('DB_PASS', 'appfactory');
+define('DB_USERNAME', 'skearch_dba');
+define('DB_PASS', get_cfg_var('SKEARCH_DBA_PASS'));
 define('DB_NAME', 'skearch_dev');
