d1b21adb58910049a7e561d58210213173c395da
Zaawar Ejaz <zaawar@yahoo.com>
2020-01-08 13:03:07 -0600

    Made linkchecker's title clickable

--- base/application/controllers/admin_panel/Linkchecker.php
+++ base/application/controllers/admin_panel/Linkchecker.php
@@ -33,7 +33,7 @@ class Linkchecker extends MY_Controller {
 		$this->load->view('admin_panel/pages/linkchecker', $data);
 	}
 
-
+	// API to get bad URLs
 	public function get_bad_urls() {
 		$urls = $this->linkcheck_model->get_bad_urls();
    		$total_urls = sizeof($urls);
@@ -50,14 +50,35 @@ class Linkchecker extends MY_Controller {
 	}
 
 	public function update_urls_status() {
-		echo "Status update started\n";
-		$urls = $this->linkcheck_model->get_urls();
-		foreach ($urls as $url) {
-			$status_code = $this->run_curl_check($url->www);
-			$this->linkcheck_model->update_http_status($url->id, $status_code);
-			echo "Url: " . $url->www . "    Status: " . $status_code;
+		ini_set('max_execution_time', 0); // to get unlimited php script execution time
+
+		if(empty($_SESSION['i'])){
+			$_SESSION['i'] = 0;
 		}
-		echo "Status update completed";
+
+		$total = 100;
+		for($i=$_SESSION['i'];$i<$total;$i++)
+		{
+			$_SESSION['i'] = $i;
+			$percent = intval($i/$total * 100)."%";   
+			
+			sleep(1); // Here call your time taking function like sending bulk sms etc.
+
+			echo '<script>
+			parent.document.getElementById("progressbar").innerHTML="<div style=\"width:'.$percent.';background:linear-gradient(to bottom, rgba(125,126,125,1) 0%,rgba(14,14,14,1) 100%); ;height:35px;\">&nbsp;</div>";
+			parent.document.getElementById("progressinfo").innerHTML="<div style=\"text-align:center; font-weight:bold\">'.$percent.' is processed.</div>";</script>';
+
+			ob_flush(); 
+			flush(); 
+		}
+		echo '<script>parent.document.getElementById("progressinfo").innerHTML="<div style=\"text-align:center; font-weight:bold\">Process completed</div>"</script>';
+
+
+		// $urls = $this->linkcheck_model->get_urls();
+		// foreach ($urls as $url) {
+		// 	$status_code = $this->run_curl_check($url->www);
+		// 	$this->linkcheck_model->update_http_status($url->id, $status_code);
+		// }
 	}
 	
 	public function run_curl_check($url) {
