fc309098262e97b4f613432e1d7d7d44eb9545bc
Iftikhar Ejaz <ejaziftikhar@gmail.com>
2020-12-17 16:16:43 -0600

    add puzzle slide captcha on sign up

--- base/application/views/auth/templates/js_global.php
+++ base/application/views/auth/templates/js_global.php
@@ -2,5 +2,4 @@
 <script src="<?= site_url(ASSETS) ?>/auth/vendors/base/vendors.bundle.js" type="text/javascript"></script>
 <script src="<?= site_url(ASSETS) ?>/auth/demo/default/base/scripts.bundle.js" type="text/javascript"></script>
 <script src="<?= site_url(ASSETS) ?>/auth/demo/default/custom/crud/forms/widgets/bootstrap-select.js" type="text/javascript"></script>
-
 <!--end::Global Theme Bundle -->
