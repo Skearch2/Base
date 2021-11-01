1f169487cabf046ddfc2fa47715acdf4dd8b98c9
Iftikhar Ejaz <ejaziftikhar@gmail.com>
2021-11-01 18:02:46 -0500

    Sprint 67 - Oct 31 2021
    Features/Changes:
    1. Show media dimensions on media upload when creating/editing ads
    2. Move "Skearch" button to Digital Assets
    3. Remove age field when signing up
    4. Registered users will not have access to Private Social

--- base/application/views/my_skearch/pages/digital_assets/default.php
+++ base/application/views/my_skearch/pages/digital_assets/default.php
@@ -138,8 +138,14 @@ $this->load->view('my_skearch/templates/start_pagebody');
 			</div>
 		</div>
 	</div>
-</div>
-<!--End::Section-->
+	<!--End::Section-->
+
+	<!--	Button - Set Skearch as homepage -->
+	<div class='home-footer-btn'>
+		<a href="#" class="btn-footer-skear"></a> <br>
+		<a href="#" title="Set Skearch as Default Search Engine" class="set-skearch">»Set Skearch as my default search engine</a>
+	</div>
+	<!--	End: Button - Set Skearch as homepage -->
 </div>
 
 <?php
